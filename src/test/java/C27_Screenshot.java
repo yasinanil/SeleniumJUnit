c3782314aba759bbd82d5f7e08f588bcba029c02
Talentify <talentify@gmail.com>
2024-09-13 16:52:45 +0300

    C27_Screenshot

---
+++ src/test/java/C27_Screenshot.java
@@ -0,0 +1,62 @@
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebElement;
+import utilities.TestBase;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class C27_Screenshot extends TestBase {
+
+    @Test
+    public void fullPageScreenshotTest() throws IOException {
+
+        driver.get("https://talentifylab.com");
+
+        File ss = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+
+        String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss_SSS").format(new Date());
+
+        File target = new File("test-outputs/screenshots/fullPageScreenshot"+timestamp+".png");
+
+        Files.copy(ss.toPath(), target.toPath());
+
+    }
+
+    @Test
+    public void specificElementsScreenshotTest() throws IOException {
+
+        driver.get("https://google.com");
+
+        File ss = driver.findElement(By.xpath("//img[@class='lnXdpd']")).getScreenshotAs(OutputType.FILE);
+
+        String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss_SSS").format(new Date());
+
+        File target = new File("test-outputs/screenshots/elementsScreenshot"+timestamp+".png");
+
+        Files.copy(ss.toPath(), target.toPath());
+
+    }
+
+    @Test
+    public void takeScreenshotUtilsTest()  {
+
+        driver.get("https://talentifylab.com");
+
+        takeFullPageScreenshotTest();
+
+        WebElement image = driver.findElement(By.xpath("(//img[@alt='Hero Background People'])[1]"));
+
+        takeSpecificElementsScreenshot(image);
+
+    }
+
+
+
+
+}
--- src/test/java/utilities/TestBase.java
+++ src/test/java/utilities/TestBase.java
@@ -2,10 +2,15 @@ package utilities;
 
 import org.junit.After;
 import org.junit.Before;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.util.Date;
 
 public class TestBase {
 
@@ -22,6 +27,38 @@ public class TestBase {
     public void tearDown() throws InterruptedException {
         Thread.sleep(3000);
         driver.quit();
+    }
+
+    public void takeFullPageScreenshotTest() {
+
+        File ss = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+
+        String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss_SSS").format(new Date());
+
+        File target = new File("test-outputs/screenshots/fullPageScreenshot"+timestamp+".png");
+
+        try {
+            Files.copy(ss.toPath(), target.toPath());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public void takeSpecificElementsScreenshot(WebElement element) {
+
+        File ss = element.getScreenshotAs(OutputType.FILE);
+
+        String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss_SSS").format(new Date());
+
+        File target = new File("test-outputs/screenshots/elementsScreenshot"+timestamp+".png");
+
+        try {
+            Files.copy(ss.toPath(), target.toPath());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
 }
